6a8c67bb342647852d4c383a8bfd7f58473827b5
Mmmmerdddd <87622756+Mmmmerdddd@users.noreply.github.com>
2024-04-30 02:39:11 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,5 @@
 export default function Header() {
   return (
-    <>
-      <footer className={styles.header}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
-      </footer>
-    </>
+west
   )
 }
